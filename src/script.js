51c83286b3f8241c59c5dffd4e2ee77c72cde198
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2022-07-21 14:34:33 +0100

    Init commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const hostForm = document.querySelector('#hostForm');
+
+async function loadChatterbox(hostname) {}
+
+window.onload = function () {};
+
+hostForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+});
